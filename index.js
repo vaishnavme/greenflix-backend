569a2776b2a885572f21b4c33175633fe239fbb6
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-05-20 21:44:40 +0530

    video route added

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const mongoose = require("mongoose");
+const { Video } = require("./models/video.models");
+
 app.use(cors());
 app.use(express.json());
 const { dbConnection } = require("./db/db.connect");
 
+const videoRouter = require("./routes/videos.route");
+
 dbConnection();
+
+app.use("/videos", videoRouter);
 
 app.get("/", (req, res) => {
     res.send("Leafshot video app api...")
 })
+
+
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
