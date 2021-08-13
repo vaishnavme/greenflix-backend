dbff27538e325739c11b9f55c2c1c633c290f159
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-08-13 17:05:13 +0530

    chores

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
-    //const token = req.headers.authorization.split(" ")[1];
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(" ")[1];
     if(!token) {
         return res.status(403).json({
             success: false,
