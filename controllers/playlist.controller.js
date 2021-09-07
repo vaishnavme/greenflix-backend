cdad23a003f79009187b39c99c3cae072fac913b
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-07 10:38:15 +0530

    update new playlist response

--- controllers/playlist.controller.js
+++ controllers/playlist.controller.js
@@ -1,51 +1,56 @@
-const { Playlist } = require("../models/playlist.model");
-const { User } = require("../models/user.model");
+const { Playlist } = require('../models/playlist.model');
+const { User } = require('../models/user.model');
+const { Video } = require('../models/video.models');
 
 const populatePlaylist = {
-    path: "playlists",
-    select: "_id playlistName",
+    path: 'playlists',
+    select: '_id playlistName',
     populate: {
-        path: "video"
+        path: 'video'
     }
-}
+};
 
-const getAllUserPlaylist = async(req, res) => {
+const getAllUserPlaylist = async (req, res) => {
     const { user } = req;
     try {
-        const userAccount = await User.findById(user.userId).populate(populatePlaylist);
+        const userAccount = await User.findById(user.userId).populate(
+            populatePlaylist
+        );
 
         res.json({
             success: true,
             playlists: userAccount.playlists
-        })
-    } catch(err) {
+        });
+    } catch (err) {
         console.log(err);
         res.json({
             success: false,
             message: `Error Occured: ${err}`
-        })
+        });
     }
-}
+};
 
-const getPlaylistById = async(req, res) => {
+const getPlaylistById = async (req, res) => {
     const { playlistId } = req.params;
     try {
-        const playlist = await Playlist.findById(playlistId).populate({path: "video"});
+        const playlist = await Playlist.findById(playlistId).populate({
+            path: 'video'
+        });
 
         res.json({
             success: true,
             playlistVideos: playlist
-        })
-    } catch(err) {
+        });
+    } catch (err) {
         console.log(err);
         res.json({
             success: false,
             message: `Error Occured: ${err}`
-        })
+        });
     }
-}
+};
 
-const createNewPlaylist = async(req, res) => {
+const createNewPlaylist = async (req, res) => {
     const { user } = req;
     const { videoId } = req.params;
     const { playlistName } = req.body;
@@ -56,52 +61,55 @@ const createNewPlaylist = async(req, res) => {
             playlistName: playlistName,
             creator: user.userId,
             video: videoId
-        })
+        });
         const playlist = await newPlaylist.save();
         userAccount.playlists.push(playlist._id);
         await userAccount.save();
-
+        const response = await Playlist.findById(playlist._id).populate({
+            path: 'video'
+        });
         res.json({
             success: true,
-            playlist
-        })
-    } catch(err) {
+            playlist: response
+        });
+    } catch (err) {
         console.log(err);
         res.json({
             success: false,
             message: `Error Occured: ${err}`
-        })
+        });
     }
-}
+};
 
-const addVideoToPlaylist = async(req, res) => {
+const addVideoToPlaylist = async (req, res) => {
     const { playlistId, videoId } = req.params;
     try {
         const playlist = await Playlist.findById(playlistId);
-        
-        if(!playlist) return res.status(404).json({
-            success: false,
-            message: "Playlist not found"
-        })
 
-        playlist.video.push(videoId)
+        if (!playlist)
+            return res.status(404).json({
+                success: false,
+                message: 'Playlist not found'
+            });
+
+        playlist.video.push(videoId);
         await playlist.save();
         res.json({
             success: true,
-            message: "Video Added",
+            message: 'Video Added',
             playlistId,
             videoId
-        })
-    } catch(err) {
+        });
+    } catch (err) {
         console.log(err);
         res.json({
             success: false,
             message: `Error Occured: ${err}`
-        })
+        });
     }
-}
+};
 
-const removeVideoFromPlaylist = async(req, res) => {
+const removeVideoFromPlaylist = async (req, res) => {
     const { playlistId, videoId } = req.params;
     try {
         const playlist = await Playlist.findById(playlistId);
@@ -111,43 +119,45 @@ const removeVideoFromPlaylist = async(req, res) => {
 
         res.json({
             success: true,
-            message: "Video removed",
+            message: 'Video removed',
             playlistId,
             videoId
-        })
-    } catch(err) {
+        });
+    } catch (err) {
         console.log(err);
         res.json({
             success: false,
             message: `Error Occured: ${err}`
-        })
+        });
     }
-}
+};
 
-const deletePlaylist = async(req, res) => {
+const deletePlaylist = async (req, res) => {
     const { user } = req;
     const { playlistId } = req.params;
     try {
         const userAccount = await User.findById(user.userId);
         const playlist = await Playlist.findByIdAndDelete(playlistId);
 
-        userAccount.playlists.splice(userAccount.playlists.indexOf(playlistId), 1);
+        userAccount.playlists.splice(
+            userAccount.playlists.indexOf(playlistId),
+            1
+        );
         await userAccount.save();
 
         res.json({
             success: true,
-            message: "Playlist is deleted",
+            message: 'Playlist is deleted',
             playlistId
-        })
-
-    } catch(err) {
+        });
+    } catch (err) {
         console.log(err);
         res.json({
             success: false,
             message: `Error Occured: ${err}`
-        })
+        });
     }
-}
+};
 
 module.exports = {
     getAllUserPlaylist,
@@ -156,4 +166,4 @@ module.exports = {
     addVideoToPlaylist,
     removeVideoFromPlaylist,
     deletePlaylist
-}
+};
